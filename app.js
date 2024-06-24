d85b7673f987bccf0433108cbdc109cf933d9281
nileshji <manifestest1@gmail.com>
2024-06-24 19:09:16 +0530

    chagnes

--- app.js
+++ app.js
@@ -32,6 +32,15 @@ app.post('/api/register', async (req, res) => {
     const { name, email, password } = req.body;
     try 
     {
+      // Check if the email already exists
+      const existingUser = await User.findOne({ where: { email } });
+      if (existingUser) 
+      {
+        return res.status(409).json({
+          message: 'Email already exists',
+        });
+      }
+
       const hashedPassword = await bcrypt.hash(password, 10);
       const user = await User.create({ name, email, password: hashedPassword });
       const token = jwt.sign({ id: user.id }, SECRET_KEY, { expiresIn: '1h' });
