186b9e0dd093b93a4bfe7fda4c969e27bd3e95e3
nileshji <manifestest1@gmail.com>
2024-06-24 18:15:44 +0530

    changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const http = require('http');
 const app = require('./app');
-const port = 8000;
+const PORT = process.env.PORT || 8000; 
 
 const server = http.createServer(app);  
-server.listen(port);
+server.listen(PORT);
